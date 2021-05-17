f2ef549788e0788ddfd3b986bc6a19cb173406bc
senseilein <73607360+senseilein@users.noreply.github.com>
2021-05-17 21:21:14 +0100

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,20 @@
 def main():
   import random
 
-  dice_rolls = 2
-
+  dice_rolls = int(input("How many dice would you like to roll?\t"))
+  dice_size = int(input("How many sides are the dices?\t"))
   dice_sum = 0
 
-  for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+  for i in range(0, dice_size):
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f"You rolled {roll}")
+
+    if roll == 1:
+      print(f"You rolled a {roll}! Critical Fail")
+    elif roll == dice_size:
+      print(f"You rolled a {dice_size}! Critical Success") # The highest number a dice can roll is the same as the number of sides
+    else:
+      print(f"You rolled a {roll}")
   print(f'You rolled a total of {dice_sum}')
 
 if __name__== "__main__":
